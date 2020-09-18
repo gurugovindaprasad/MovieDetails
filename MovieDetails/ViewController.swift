7bd4b0c7acfb70c4d9fe0bfc443c902e12603bd7
gurugovindaprasad <gurugovindaprasad@gmail.com>
2020-09-18 13:40:18 +0530

    Initial Commit

---
+++ MovieDetails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDetails
+//
+//  Created by Guru Ranganathan on 9/18/20.
+//  Copyright © 2020 Guru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
